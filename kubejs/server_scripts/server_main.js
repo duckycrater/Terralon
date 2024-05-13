84ebb77eb325052b7b8795aaaab7dc72b695a617
FayneTheKnight <faynetheknight@gmail.com>
2024-05-12 20:01:38 -0500

    added initial inventory big book

--- kubejs/server_scripts/server_main.js
+++ kubejs/server_scripts/server_main.js
@@ -13,4 +13,26 @@ ServerEvents.recipes(e=>{
         N: 'powah:capacitor_nitro',
         S: '#c:steel_plates'
     })
+    e.shaped('tempad:he_who_remains_tempad', [
+        'SPS',
+        'THU',
+        'SCS'
+    ], {
+        T: 'tempad:tempad',
+        H: 'modern_industrialization:highly_advanced_machine_hull',
+        P: 'modern_industrialization:processing_unit',
+        U: 'modern_industrialization:highly_advanced_upgrade',
+        C: 'powah:energy_cell_nitro',
+        S: '#c:titanium_plates'
+    })
+})
+
+PlayerEvents.loggedIn(e=>{
+    // Check if player doesn't have "starting_items" stage yet
+    if (!e.player.stages.has('starting_items')) {
+        // Add the stage
+        e.player.stages.add('starting_items')
+        // Give some items to player
+        e.player.give(Item.of('eccentrictome:tome', '{"eccentrictome:mods":{bewitchment:{0:{Count:1b,id:"bewitchment:book_of_shadows"}},botania:{0:{Count:1b,id:"botania:lexicon"}},haema:{0:{Count:1b,id:"haema:book_of_blood"}},hexcasting:{0:{Count:1b,id:"patchouli:guide_book",tag:{"patchouli:book":"hexcasting:thehexbook"}}},modern_industrialization:{0:{Count:1b,id:"modern_industrialization:guidebook"}},powah:{0:{Count:1b,id:"powah:book"}},spectrum:{0:{Count:1b,id:"spectrum:guidebook"}}}}').enchant('yigd:soulbound', 1))
+    }
 })
