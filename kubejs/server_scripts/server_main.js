c32db33e86c6300b2e6105d424e60dbb61e43853
FayneTheKnight <faynetheknight@gmail.com>
2024-05-15 16:07:44 -0500

    robot repair stuff

--- kubejs/server_scripts/server_main.js
+++ kubejs/server_scripts/server_main.js
@@ -29,37 +29,54 @@ ServerEvents.recipes(e=>{
         S: '#c:titanium_plates'
     })
 
-    //e.replaceOutput({output:'minecraft:end_portal_frame'})
+    e.shapeless('3x kubejs:light_repair_pack', [
+        '3x #c:nuggets',
+        '2x #c:ingots'
+    ])
+    e.shapeless('4x kubejs:robust_repair_pack', [
+        '#c:gears',
+        '3x #c:plates',
+        '3x #c:ingots',
+        '2x #c:nuggets'
+    ])
+    e.shaped('kubejs:robot_upgrade_kit', [
+        'III',
+        'GCG',
+        'GGG'
+    ], {
+        C: 'kubejs:robot_upgrade_core',
+        I: '#c:iron_plates',
+        G: 'minecraft:gold_ingot'
+    })
 })
 
 ServerEvents.customCommand('origin', event => {
     event.server.runCommand('origin gui ' + event.player.username)
 })
 
+ServerEvents.customCommand('the_book', e=>{
+    e.player.give(Item.of('eccentrictome:tome', '{"eccentrictome:mods":{ae2:{0:{Count:1b,id:"ae2:guide"}},bewitchment:{0:{Count:1b,id:"bewitchment:book_of_shadows"}},botania:{0:{Count:1b,id:"botania:lexicon"}},haema:{0:{Count:1b,id:"haema:book_of_blood"}},hexcasting:{0:{Count:1b,id:"patchouli:guide_book",tag:{"patchouli:book":"hexcasting:thehexbook"}}},modern_industrialization:{0:{Count:1b,id:"modern_industrialization:guidebook"}},powah:{0:{Count:1b,id:"powah:book"}},spectrum:{0:{Count:1b,id:"spectrum:guidebook"}}},"eccentrictome:version":1}').enchant('yigd:soulbound', 1))
+})
+
 ServerEvents.tags('item', e=>{
-    e.add('fayne_origins:metal_repair_resources','minecraft:iron_nugget')
-    e.add('fayne_origins:metal_repair_resources','minecraft:copper_ingot')
-    e.add('fayne_origins:metal_repair_resources','minecraft:gold_ingot')
-    e.add('fayne_origins:strong_metal_repair_resources','minecraft:iron_ingot')
-    e.add('fayne_origins:strong_metal_repair_resources','modern_industrialization:titanium_ingot')
+    e.add('fayne_origins:metal_repair_resources','kubejs:light_repair_pack')
+    e.add('fayne_origins:strong_metal_repair_resources','kubejs:robust_repair_pack')
     e.add('fayne_origins:energy_sources','minecraft:coal')
     e.add('fayne_origins:energy_sources','minecraft:charcoal')
     e.add('fayne_origins:energy_sources','modern_industrialization:lignite_coal')
     e.add('fayne_origins:energy_sources','minecraft:redstone')
     e.add('fayne_origins:energy_sources','minecraft:glowstone_dust')
     e.add('fayne_origins:energy_sources','minecraft:blaze_powder')
-    e.add('fayne_origins:high_energy_sources','minecraft:blaze_rod')
-    e.add('fayne_origins:upgrade_items','minecraft:nether_star')
+    //e.add('fayne_origins:high_energy_sources','minecraft:blaze_rod')
+    //e.add('fayne_origins:high_energy_sources','modern_industrialization:fuel_rod')
+    e.add('fayne_origins:upgrade_items','kubejs:robot_upgrade_kit')
     e.add('fayne_origins:upgrade_items','modern_industrialization:advanced_upgrade')
 })
 
 PlayerEvents.loggedIn(e=>{
-    // Check if player doesn't have "starting_items" stage yet
     if (!e.player.stages.has('starting_items')) {
-        // Add the stage
         e.player.stages.add('starting_items')
-        e.server.scheduleInTicks(1 * 50, callback => {
-            //e.player.inventory.clear()
+        e.server.scheduleInTicks(50, callback => {
             e.player.give(Item.of('eccentrictome:tome', '{"eccentrictome:mods":{bewitchment:{0:{Count:1b,id:"bewitchment:book_of_shadows"}},botania:{0:{Count:1b,id:"botania:lexicon"}},byg:{0:{Count:1b,id:"byg:biomepedia"}},haema:{0:{Count:1b,id:"haema:book_of_blood"}},hexcasting:{0:{Count:1b,id:"patchouli:guide_book",tag:{"patchouli:book":"hexcasting:thehexbook"}}},modern_industrialization:{0:{Count:1b,id:"modern_industrialization:guidebook"}},powah:{0:{Count:1b,id:"powah:book"}},spectrum:{0:{Count:1b,id:"spectrum:guidebook"}}}}').enchant('yigd:soulbound', 1))
         });
     }
@@ -101,13 +118,13 @@ removeAdvancement([
     'haema:advancements/root',
     'haema:advancements/store_blood',
     'haema:advancements/trigger_vampire_hunters',
-'haema:advancements/use_invis',
-'haema:advancements/use_dash',
-'haema:advancements/use_mist',
-'haema:advancements/use_ritual_table',
-'haema:advancements/fulfil_contract',
-'haema:advancements/create_ritual_table',
-'haema:advancements/get_contract',
-'hexcasting:advancements/grant_patchi_book',
+    'haema:advancements/use_invis',
+    'haema:advancements/use_dash',
+    'haema:advancements/use_mist',
+    'haema:advancements/use_ritual_table',
+    'haema:advancements/fulfil_contract',
+    'haema:advancements/create_ritual_table',
+    'haema:advancements/get_contract',
+    'hexcasting:advancements/grant_patchi_book',
 ])
 
