d84dde1945f3f86d97d66b8b49021a5c06ef003a
FayneTheKnight <faynetheknight@gmail.com>
2024-05-20 08:52:12 -0500

    added incomplete robot upgrade kit

--- kubejs/startup_scripts/startup_main.js
+++ kubejs/startup_scripts/startup_main.js
@@ -2,6 +2,7 @@
 StartupEvents.registry('item', e => {
     e.create('robot_upgrade_core').texture('fayne_origins:item/robot_upgrade_core')
     e.create('robot_upgrade_kit').texture('fayne_origins:item/robot_upgrade_kit')
+    e.create('incomplete_robot_upgrade_kit').texture('fayne_origins:item/incomplete_robot_upgrade_kit')
     e.create('light_repair_pack').texture('fayne_origins:item/light_repair_pack')
     e.create('robust_repair_pack').texture('fayne_origins:item/robust_repair_pack')
 })
