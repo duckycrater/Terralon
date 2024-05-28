97d8a6e4fe3ec2ede3185b894c763492a2f0fe3e
tilles111 <59595469+tilles111@users.noreply.github.com>
2024-05-28 12:34:22 -0700

    Galvanized Square Steel

--- kubejs/startup_scripts/startup_main.js
+++ kubejs/startup_scripts/startup_main.js
@@ -5,4 +5,8 @@ StartupEvents.registry('item', e => {
     e.create('incomplete_robot_upgrade_kit').texture('fayne_origins:item/incomplete_robot_upgrade_kit')
     e.create('light_repair_pack').texture('fayne_origins:item/light_repair_pack')
     e.create('robust_repair_pack').texture('fayne_origins:item/robust_repair_pack')
+    e.create('galvanized_square_steel').texture('kubejs:item/galvanized_square_steel')
+    e.create('galvanized_steel_rod').texture('kubejs:item/galvanized_steel_rod')
+    e.create('iron_screw').texture('kubejs:item/iron_screw')
+    e.create('expansion_screw_from_your_aunt').texture('kubejs:item/expansion_screw_from_your_aunt')
 })
